d82f6fef04768e3ffaab5a68f6d442939ffadf7b
Edwin Salazar <egs25@cornell.edu>
2018-01-14 22:58:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,5 +21,7 @@ function sayHiToGrandma(string){
   else if(string.toLowerCase()===string){
     return "I can't hear you!"
   }
-  
+  else if(string.toUpperCase()===string){
+    return "YES INDEED!"
+  }
 }
